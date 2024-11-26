b7df854f65a69415c3505b70be1f4e85895481ef
Linux Hari <vadiveluversionz1.0@gmail.com>
2024-11-26 23:07:12 +0530

    feat: stripe

--- src/services/stripeService.ts
+++ src/services/stripeService.ts
@@ -1,3 +1,21 @@
-export const stripeCreate = () => {
-    
+import Stripe from "stripe"
+
+type StripeData = {
+    amount: number
+    currency: "USD" | "INR"
+    bookingId: string
+    userId: string
+}
+
+const stripe =  new Stripe(process.env.STRIPE_SECRET as string)
+
+export const stripeCreate = async ({amount, currency, bookingId, userId}: StripeData) => {
+    const chargeDetails = await stripe.charges.create({
+        amount,
+        currency,
+        metadata: {
+            bookingId,
+            userId
+        }
+    })
 }
